c831484806a1bfa797671ea43f0de8447c84ecf0
Ndeye Astou Niang <niangndeyeastou578@gmail.com>
2026-06-19 23:15:15 +0000

    Ajout connexion MYSQL

---
+++ Data_base/config.py
@@ -0,0 +1,10 @@
+TYPE_BD = "mysql" # Oubien postgres
+
+MYSQL = {
+    "host" : "localhost",
+    "port" : "3306",
+    "database" : "gestion_commande",
+    "user" : "root",
+    "password" : ""
+}
+
